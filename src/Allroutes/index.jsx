ae33fd22a82406be4d4743353a5ecd8bbcee594f
Muhammad Maaz <maazm6387@gmail.com>
2024-11-13 22:50:27 +0500

    addition

---
+++ src/Allroutes/index.jsx
@@ -0,0 +1,14 @@
+// import HomePage from "../Pages/Home";
+import Registration from "../Pages/Register";
+import LoginPage from "../Pages/Login";
+
+const AllRoutes = () => {
+
+    return (
+        <div>
+        
+
+        </div>
+        );
+}
+export default AllRoutes;
